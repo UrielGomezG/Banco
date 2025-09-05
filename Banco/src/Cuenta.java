d84cfb58a978606ba86fe157d2b1f3cb8a985574
UrielGomezG <20233tn177@utez.edu.mx>
2025-09-04 19:50:27 -0600

    creacion del objeto de la cuenta con el metodo de depositar

--- Banco/src/Cuenta.java
+++ Banco/src/Cuenta.java
@@ -1,3 +1,23 @@
 public class Cuenta {
     private double saldo;
+
+    public Cuenta(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void depositar(double cantidad) {
+        {
+            saldo = saldo + cantidad;
+        }
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
 }
